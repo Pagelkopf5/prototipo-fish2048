e6e2b125c89cce029f18fc3cf72fdb7fbcca560e
Gabriel Pagelkopf <gabriel.pagelkopf@gmail.com>
2019-04-07 15:37:23 -0300

    test build

---
+++ docs/precache-manifest.69cf4254b1dc6a15c2192e29f0c81514.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c87b95148e3dbdb22028",
+    "url": "/static/js/main.a43a37fb.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "c87b95148e3dbdb22028",
+    "url": "/static/css/main.dfc49d81.chunk.css"
+  },
+  {
+    "revision": "7155a45a7aa161d25c4ce754bff59873",
+    "url": "/index.html"
+  }
+];
